e886a5dcdbefae0de4bd406cb03405194c6e12f0
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-22 15:47:29 +0530

    Add files via upload

---
+++ Numpy/num8.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+an_array = np.array([[1, 2], [3, 4]])
+another_array = np.array([[1, 2], [3, 4]])
+
+comparison = an_array == another_array
+equal_arrays = comparison.all()
+
+print(equal_arrays)
