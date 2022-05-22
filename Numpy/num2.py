e8718e16d1bd3b2b00d1b0d74c05634b1e0f6fe3
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-22 15:37:26 +0530

    Add files via upload

---
+++ Numpy/num2.py
@@ -0,0 +1,10 @@
+# import the important module in python
+import numpy as np
+		
+# make a matrix with numpy
+gfg = np.matrix('[1, 2, 3; 4, 5, 6; 7, 8, 9]')
+		
+# applying matrix.max() method
+geeks = gfg.max()
+	
+print(geeks)
