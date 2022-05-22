7a21309e8b1a18f8592b701d309b655d983ec046
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-22 15:34:40 +0530

    Delete a.py

--- Numpy/a.py
+++
@@ -1,8 +0,0 @@
-# Python Program to create array with all zeros
-import numpy as np
-
-a = np.zeros(3, dtype = int)
-print("Matrix a : \n", a)
-
-b = np.zeros([3, 3], dtype = int)
-print("\nMatrix b : \n", b)
