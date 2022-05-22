b57fb266b2a807c78cd49b997a57576095ed8318
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-22 15:48:47 +0530

    Add files via upload

---
+++ Numpy/num9.py
@@ -0,0 +1,9 @@
+# importing numpy as np
+import numpy as np
+
+# declare matrix with np
+gfg = np.array([[2, 3], [4, 5]])
+
+# using array.flatten() method
+flat_gfg = gfg.flatten()
+print(flat_gfg)
