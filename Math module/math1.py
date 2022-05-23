f774bb7c0da085445d19ed453e3e5eed68a4ce5a
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-23 22:37:04 +0530

    Add files via upload

---
+++ Math module/math1.py
@@ -0,0 +1,9 @@
+# Import math Library
+import math
+
+# Return the arc cosine of numbers
+print(math.acos(0.55))
+print(math.acos(-0.55))
+print(math.acos(0))
+print(math.acos(1))
+print(math.acos(-1))
