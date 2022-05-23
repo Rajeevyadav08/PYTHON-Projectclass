dafda26061bf83ea075b0712cf96891f0a867392
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-23 22:40:22 +0530

    Add files via upload

---
+++ Math module/math3.py
@@ -0,0 +1,9 @@
+# Import math library
+import math
+
+# Round a number upward to its nearest integer
+print(math.ceil(1.4))
+print(math.ceil(5.3))
+print(math.ceil(-5.3))
+print(math.ceil(22.6))
+print(math.ceil(10.0))
