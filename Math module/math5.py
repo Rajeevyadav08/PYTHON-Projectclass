baee9c5a4d47579cf885008627f409c72996e502
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-23 22:40:59 +0530

    Add files via upload

---
+++ Math module/math5.py
@@ -0,0 +1,9 @@
+# Import math Library
+import math
+
+# Return the gamma function for different numbers
+print(math.gamma(-0.1))
+print(math.gamma(8))
+print(math.gamma(1.2))
+print(math.gamma(80))
+print(math.gamma(-0.55))
