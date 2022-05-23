2ced596a6e482becae82bbde78a029bb14217f16
Rajeev Yadav <103309049+Rajeevyadav08@users.noreply.github.com>
2022-05-23 22:40:02 +0530

    Add files via upload

---
+++ Math module/math2.py
@@ -0,0 +1,9 @@
+# Import math Library
+import math
+
+# Return the arc sine of numbers
+print(math.asin(0.55))
+print(math.asin(-0.55))
+print(math.asin(0))
+print(math.asin(1))
+print(math.asin(-1))
